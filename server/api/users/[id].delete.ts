091fc22c68e19196ef9cafad3cbd233d568c2096
Realitaa <rezamuliaputra927@gmail.com>
2026-04-02 08:23:55 +0700

    fix: restrict self delete

--- server/api/users/[id].delete.ts
+++ server/api/users/[id].delete.ts
@@ -1,11 +1,18 @@
 import { deleteUser } from "#server/services/user.service";
 import { ApiResponse } from "#server/utils/response";
+import { BadRequestError } from "#server/utils/exceptions";
 
 export default defineEventHandler(async (event) => {
   try {
     const id = getRouterParam(event, "id");
 
-    await deleteUser(id!);
+    // check if user is the same as the logged in user
+    const { user } = await requireUserSession(event)
+    if (user.id === id) {
+      throw new BadRequestError({ id: ["Anda tidak dapat menghapus diri sendiri."] }, "Tidak dapat menghapus diri sendiri.");
+    }
+
+    await deleteUser(id);
 
     return ApiResponse.success(null, "Pengguna berhasil dihapus.");
   } catch (e) {
