4ab792e5b83afe2c522b9a71b68fc38ef509a185
Realitaa <rezamuliaputra927@gmail.com>
2026-04-04 17:01:24 +0700

    fix: path in profile picture setting

--- app/components/setting/ProfilePicture.vue
+++ app/components/setting/ProfilePicture.vue
@@ -5,23 +5,13 @@ const { currentPicture, profilePicture, updateProfilePicture } =
 
 <template>
   <div class="space-y-4">
-    <UPageFeature
-      title="Pengaturan Foto Profil"
-      description="Perbarui pengaturan foto profil untuk akun Anda dari set foto yang tersedia."
-    />
+    <UPageFeature title="Pengaturan Foto Profil"
+      description="Perbarui pengaturan foto profil untuk akun Anda dari set foto yang tersedia." />
     <div class="flex gap-4">
-      <UAvatar
-        v-for="pict in profilePicture"
-        :src="pict.src"
-        size="2xl"
-        class="ring-3 cursor-pointer"
-        :class="
-          pict.id === currentPicture
-            ? 'ring-primary'
-            : 'ring-slate-300 dark:ring-slate-700 hover:ring-slate-500'
-        "
-        @click="updateProfilePicture(pict.id)"
-      />
+      <UAvatar v-for="pict in profilePicture" :src="`/img/${pict.src}`" size="2xl" class="ring-3 cursor-pointer" :class="pict.id === currentPicture
+        ? 'ring-primary'
+        : 'ring-slate-300 dark:ring-slate-700 hover:ring-slate-500'
+        " @click="updateProfilePicture(pict.id)" />
     </div>
   </div>
 </template>
